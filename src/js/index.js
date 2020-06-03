b6df1050f3aa6232c02d3c1f5703479f6573de17
Dylan Hazard <dylan.t.hazard@gmail.com>
2020-06-02 21:01:02 -1000

    Install and set up babel
    -create new config file for babel called .babelrc
    
    -update the webpack config file for babel

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 // Global app controller
 import x from './test'
+const num = 23
 
-console.log(x + ' is imported from test.js')
+console.log(`${x} is imported from test.js, var num is ${num}`);
 // alert('hello')
