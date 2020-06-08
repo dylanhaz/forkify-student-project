e59e5c2fa50c2ec191cb44038d96d82de493d9c0
Dylan Hazard <dylan.t.hazard@gmail.com>
2020-06-08 13:37:47 -1000

    Build the Recipe model
    -Tested in index.js with hardcoded id, seems to be working

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
 import { elements, renderLoader, clearLoader} from './views/base';
 
@@ -10,6 +11,10 @@ import { elements, renderLoader, clearLoader} from './views/base';
  */
 const state = {};
 
+
+/**
+ * SEARCH CONTROLLER
+ */
 const controlSearch = async () => {
     // 1) Get query from view
     const query = searchView.getInput();
@@ -49,3 +54,12 @@ elements.searchResPages.addEventListener('click', e => {
         searchView.renderResults(state.search.result, goToPage);
     }
 });
+
+/**
+ * RECIPE CONTROLLER
+ */
+
+ const r = new Recipe('47746');
+ r.getRecipe();
+
+ console.log(r)
