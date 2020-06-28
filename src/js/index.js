d5060fe1bac3669cd72394eebb88e95767614b40
Dylan Hazard <dylan.t.hazard@gmail.com>
2020-06-27 16:32:20 -1000

    Build the likes model and controller

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader} from './views/base';
+import Likes from './models/Likes';
 
 /** Global state of the app
  * -Search object
@@ -140,39 +141,44 @@ const controlRecipe = async () => {
  
 
 
- //Handling recipe button clicks
- elements.recipe.addEventListener('click', e => {
-    
-     if (e.target.matches('.btn-decrease *')) {
-         if (state.recipe.servings > 1) {
-             state.recipe.updateServings('dec');
-             recipeView.updateServingsIngredients(state.recipe);
-         }
-        // Decrease button is clicked
-     } else if (e.target.matches('.btn-increase *')) {
-        // Increase button is clicked
-        state.recipe.updateServings('inc');
-        recipeView.updateServingsIngredients(state.recipe);
-     } else if(e.target.matches('.recipe__btn--add *')) {
-        if(!state.list){
-            state.list = new List();
-        }
-         //Add to shopping cart
-        //  console.log(state.recipe);
-         //Clear the UI
-         elements.shopping.innerHTML = '';
-         state.recipe.ingredients.forEach(e => {
-             state.list.addItem(e.count, e.unit, e.ingredient);
-         });
-         //Check for duplicates
-         state.list.checkForDuplicates();
-      
-         state.list.items.forEach(e => {
-            listView.renderItem(e);
-         });
-     }
 
- });
+
+ /**
+  * LIKE CONTROLLER
+  */
+ const controlLike = () => {
+    if (!state.likes) state.likes = new Likes();
+    const currentID = state.recipe.id;
+    //User has NOT yet liked recipe
+    if (!state.likes.isLiked(currentID)) {
+        //Add like to the state
+        //id, title, author, img
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img
+        );
+        // Toggle the like button
+
+        // Add like to UI list
+        console.log(state.likes)
+
+    //User HAS liked recipe
+    } else {
+        //Remove like from the state
+        state.likes.deleteLike(currentID);
+        //Toggle the like button
+
+        // Remove like from UI list
+        console.log(state.likes)
+    }
+ };
+
+
+ /**
+  * SHOPPING LIST CONTROLLER
+  */
 
  elements.shopping.addEventListener('click', e => {
     //Delete recipe Item
@@ -199,6 +205,43 @@ const controlRecipe = async () => {
          e.target.value = 0;
      };
  });
+
+  //Handling recipe button clicks
+  elements.recipe.addEventListener('click', e => {
+    
+    if (e.target.matches('.btn-decrease *')) {
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+       // Decrease button is clicked
+    } else if (e.target.matches('.btn-increase *')) {
+       // Increase button is clicked
+       state.recipe.updateServings('inc');
+       recipeView.updateServingsIngredients(state.recipe);
+    } else if(e.target.matches('.recipe__btn--add *')) {
+       if(!state.list){
+           state.list = new List();
+       }
+        //Add to shopping cart
+       //  console.log(state.recipe);
+        //Clear the UI
+        elements.shopping.innerHTML = '';
+        state.recipe.ingredients.forEach(e => {
+            state.list.addItem(e.count, e.unit, e.ingredient);
+        });
+        //Check for duplicates
+        state.list.checkForDuplicates();
+     
+        state.list.items.forEach(e => {
+           listView.renderItem(e);
+        });
+    } else if(e.target.matches('.recipe__love *')) {
+        //Like controller
+        controlLike();
+    }
+
+});
      
 
 // window.l = new List();
--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -19,8 +19,3 @@ export const renderItem = item => {
     elements.shopping.insertAdjacentHTML('beforeend', markup);
 
 };
-
-// export const deleteItem = id => {
-//     const item = document.querySelector(`[data-itemid="${id}"]`);
-//     item.parentElement.removeChild(item);
-// };
